3db5327482b3b918901c32ff48103a463e114c95
didikmulyadi <didikmulyadi12@gmail.com>
2021-09-07 18:58:08 +0700

    Added roboto as a default font

--- src/Definitions/Theme/theme.ts
+++ src/Definitions/Theme/theme.ts
@@ -1,22 +1,25 @@
-import { createTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+    typography: {
+        fontFamily: "Roboto, arial, calibri",
     },
-    secondary: {
-      main: '#19857b',
+    palette: {
+        primary: {
+            main: "#556cd6",
+        },
+        secondary: {
+            main: "#19857b",
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: "#fff",
+        },
     },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
 });
 
 export default theme;
