19780f9a1c851d7de5d634fe361dd8505b8a586a
didikmulyadi <didikmulyadi12@gmail.com>
2021-09-07 10:56:16 +0700

    Added React.memo in button component

--- src/Components/Atoms/Button/Button.interface.ts
+++ src/Components/Atoms/Button/Button.interface.ts
@@ -1,7 +1,7 @@
 import Button, { ButtonProps } from '@material-ui/core/Button';
 
 export default interface IButton extends ButtonProps {
-        
+    
 }
 
 
--- src/Components/Atoms/Button/index.tsx
+++ src/Components/Atoms/Button/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Button from '@material-ui/core/Button';
+import React, { memo } from "react";
+import Button from "@material-ui/core/Button";
 
 import IButton from "./Button.interface";
 
-export const ButtonExample = (props: IButton) => {
+export const ButtonExample = memo((props: IButton) => {
     return <Button {...props} />;
-};
+});
