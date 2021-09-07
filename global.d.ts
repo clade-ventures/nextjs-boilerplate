6ad573f0d428107cae625473ea5ce6541fa507e2
didikmulyadi <didikmulyadi12@gmail.com>
2021-09-07 18:45:54 +0700

    Changed default env

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,7 @@
 declare namespace Process {
     interface ProcessEnv {
-        PROXY_MODE: string;
-        NEXT_PUBLIC_STATIC_PATH: string;
         NEXT_PUBLIC_API_URL: string;
-        NEXT_PUBLIC_API_KEY: string;
+        BUNDLE_ANALYZE: string;
     }
 }
 
