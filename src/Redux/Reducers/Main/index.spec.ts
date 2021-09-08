ee3b5ae750c4e3a01a31f0b1eb21a5734b898a79
didikmulyadi <didikmulyadi12@gmail.com>
2021-09-08 10:37:19 +0700

    Added example test to 100% cover the file

--- src/Redux/Reducers/Main/index.spec.ts
+++ src/Redux/Reducers/Main/index.spec.ts
@@ -13,6 +13,42 @@ describe("Main Reducer", () => {
         });
     });
 
+    it("should return empty string when payload is empty", () => {
+        expect(
+            CounterReducer(undefined, {
+                type: ActionConsts.Main.SetLoginDataReducer,
+            })
+        ).toEqual({
+            token: "",
+        });
+    });
+
+    it("should return the initial state by param", () => {
+        expect(
+            CounterReducer(
+                {
+                    token: "",
+                },
+                {} as IAction<IMain.Actions.IPayload>
+            )
+        ).toEqual({
+            token: "",
+        });
+    });
+
+    it("should handle Set Login Data", () => {
+        expect(
+            CounterReducer(undefined, {
+                payload: {
+                    token: "12345678910",
+                },
+                type: ActionConsts.Main.SetLoginDataReducer,
+            })
+        ).toEqual({
+            token: "12345678910",
+        });
+    });
+
     it("should handle Reset Login Data", () => {
         expect(
             CounterReducer(undefined, {
