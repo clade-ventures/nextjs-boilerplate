60f43de5db1ba0ce2d6f14824b55188867d330e5
didikmulyadi <didikmulyadi12@gmail.com>
2021-09-08 10:36:19 +0700

    Added prefix validation in ButtonProps

--- src/Components/Atoms/Button/Button.interface.ts
+++ src/Components/Atoms/Button/Button.interface.ts
@@ -2,6 +2,7 @@ import { ButtonProps } from "@material-ui/core/Button";
 
 export default interface IButton extends ButtonProps {
     gradientColor?: "red" | "blue";
+    testID: `button-${string}`;
 }
 
 export interface IStyle {
